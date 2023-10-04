6edf391ee9f53dc9f6c036c4cfb6173ddbc95245
MihaelaBoian <pinzari_m@mail.ru>
2023-10-04 11:46:54 +0300

    modif

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Circle {
+//am creat clasaCircle
+public class Circle {
+//am definit un atribut
+  double radius ;
+  public Circle(double radius){
+        this.radius=radius;
+    }
+  // am definit o metoda
+   public  double calculateArea()
+   { return Math.PI * radius * radius ;}
+//definim constructor
+
+
 }
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+      //  Circle ariaMea = new Circle();
+
+        // System.out.println("Aria=" + ariaMea.calculateArea());
+
+        Circle ariaNoua = new Circle(3);
+
+        System.out.println("Aria noua =" + ariaNoua.calculateArea());
+
+
     }
+
 }
